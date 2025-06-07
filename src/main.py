0e515f52d1a7061e5577945b35b3313d448ce762
ceciliafernandest <ana.fernandes.rj@gmail.com>
2025-06-07 16:25:13 +0100

    Start
    Adiciona main.py inicial com mensagem de boas-vindas

---
+++ src/main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Bem-vinda ao Atlas CRM!")
+    print("A sua ferramente para gerenciar Pipeline de Vendas, Sucesso do Cliente e Área Financeira.")
+
+if __name__ == "__main__":
+    main()
